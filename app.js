bed64f68c00002189b48ae289789a6ae610c7589
yidye <yidye@pop-os.localdomain>
2022-05-24 10:25:03 +0300

    final project

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const morgan = require('morgan')
+const methodOverride = require('method-override')
 
 // Load config
 dotenv.config({path: './config/config.env'})
@@ -20,15 +21,23 @@ const app = express()
 //body parser
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-
+//Method overriders
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 //handlebar helpers
-const {formatDate} = require('./helpers/hbs')
+const {formatDate, truncate, stripTags,editIcon, select} = require('./helpers/hbs')
 //handlebars config
 app.engine('.hbs', engine({helpers:{
-  formatDate
+  formatDate, truncate, stripTags, editIcon, select
 },defaultLayout:'main',extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
@@ -46,6 +55,11 @@ app.use(
 //passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+//set Global variable
+app.use(function(req,res,next){
+  res.locals.user = req.user || null
+  next()
+})
 //static folders
 app.use(express.static(path.join(__dirname, 'public')))
 //Routes
